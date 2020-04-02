8dd3406ffe18a87c39ab8c8f97f82af05b582d76
nadir <marouane.nadir@outlook.it>
2020-04-03 00:27:18 +0200

    Class for execution of the proof experiments

---
+++ src/main/java/org/liveontologies/puli/pinpointing/PrunedProofComputation.java
@@ -0,0 +1,84 @@
+package org.liveontologies.puli.pinpointing;
+
+/*-
+ * #%L
+ * Proof Utility Library
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2020 Live Ontologies Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.util.Set;
+
+import org.liveontologies.puli.Inference;
+import org.liveontologies.puli.Proof;
+import org.liveontologies.puli.Proofs;
+
+public class PrunedProofComputation <C, I extends Inference<? extends C>> {
+
+	
+	private final PruneType pruneType_;
+	private Proof<? extends I> proof_;
+	private final C query;
+	private final Set<C> ontology;
+	private final Set<Object> justUnion;
+
+
+	
+	public PrunedProofComputation(final Proof<? extends I> proof,
+			final InterruptMonitor monitor, final PruneType pruneType,final C query,final Set<C> ontology,Set<Object> just) {
+		
+		this.proof_ = proof;
+		this.pruneType_=pruneType;
+		this.query=query;
+		this.ontology=ontology;
+		this.justUnion=just;
+	}
+	
+	public enum PruneType {
+		NO_PRUNE, ESS_PRUNE, CYC_PRUNE,ESSCYC_PRUNE,JUST_PRUNE;
+		// TODO: switch to class hierarchy to support threshold with parameter
+	}
+	
+	public Proof<? extends I> computePrune(){
+		
+		switch (pruneType_) {
+		case ESS_PRUNE:
+				proof_=Proofs.prune(proof_, query, ontology);
+			break;
+		case CYC_PRUNE:
+				proof_=Proofs.pruneCycle(proof_, query, ontology);
+			break;
+			
+		case ESSCYC_PRUNE:
+			proof_=Proofs.prune(proof_, query, ontology);
+			proof_=Proofs.pruneCycle(proof_, query, ontology);
+			break;
+		case JUST_PRUNE:
+			proof_=Proofs.prune(proof_, query, ontology);
+			proof_=Proofs.pruneCycle(proof_, query, ontology);
+			proof_=Proofs.pruneFromJustifications(proof_, query, justUnion);
+			break;
+		
+		default:
+			break;
+		}	
+		return proof_;
+	}
+	
+}
