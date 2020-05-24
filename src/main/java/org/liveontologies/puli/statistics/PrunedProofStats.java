93b08cb66d21b3eed56091c52c3b0715f929e9c7
nadir <nadir@nadir-desktop>
2020-05-24 18:44:44 +0200

    add pruning strategies statistics

---
+++ src/main/java/org/liveontologies/puli/statistics/PrunedProofStats.java
@@ -0,0 +1,64 @@
+package org.liveontologies.puli.statistics;
+
+/*-
+ * #%L
+ * Proof Utility Library
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2020 Live Ontologies Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.liveontologies.puli.Inference;
+import org.liveontologies.puli.Proof;
+import org.liveontologies.puli.Proofs;
+import org.liveontologies.puli.PrunedProofCycle;
+import org.liveontologies.puli.PrunedProofEssential;
+
+public class PrunedProofStats<C, I extends Inference<? extends C>> {
+
+	private Proof<? extends I> proof_;
+	private final C query;
+	PrunedProofEssential<C, Inference<? extends C>> pruneEss;
+
+	public PrunedProofStats(final Proof<? extends I> proof, C query) {
+
+		this.proof_ = proof;
+		this.query = query;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public int computeEss() {
+		pruneEss = (PrunedProofEssential<C, Inference<? extends C>>) Proofs
+				.pruneEssential(proof_, query);
+		return pruneEss.getEssential().size();
+
+	}
+
+	public int computeDerivEss() {
+		return pruneEss.getDerivable().size();
+	}
+
+	public int computeInfCycl() {
+		@SuppressWarnings("unchecked")
+		PrunedProofCycle<C, Inference<? extends C>> pruneCyc = (PrunedProofCycle<C, Inference<? extends C>>) Proofs
+				.pruneCycle(proof_, query);
+		return pruneCyc.getInferenceCyc().size();
+
+	}
+
+}
